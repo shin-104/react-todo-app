4e355d0e75261f5914c7b81f4c3777feee6c0078
shin-note <Shin.studio104@gmail.com>
2022-07-31 23:01:44 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+
+type Todo = {
+  value: string
+}
 
 function App() {
+
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleOnSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+    setText('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleOnSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={(e) => handleOnChange(e)} />
+        <input type="submit" value="追加" onSubmit={handleOnSubmit} />
+      </form>
+      <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.value}</li>;
+        })}
+      </ul>
     </div>
   );
 }
